88089620d4ca4ffabbfce3aaf41e215619e6e609
NikonovichN <nikonovichnd@gmail.com>
2019-03-07 09:17:21 +0300

    fix: search a bad BRACKETS

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ module.exports = function check(str, bracketsConfig) {
     return false;
   }
 
-  var count = 0, stack = [];
+  var count = 0, stack = [], positionBadBrackets = [];
   for( var i = 0; i < sLength; i++ ){
     for( var k = 0; k < bracketsConfig.length; k++ ){
       if( bracketsConfig[k][0] == bracketsConfig[k][1] && bracketsConfig[k][0] == str[i] ){
-
+        for( var j = i; j < sLength; j++ ){
+          if( bracketsConfig[k][0] == str[i] ){
+            for( var z = j; z < sLength; z++ ){
+              if( str[z] == str[j] && (z + j + 1) % 2 == 0 ){
+                str = setCharAt(str,j,'0');
+                str = setCharAt(str,z,'0');
+                break;
+              }
+            }
+          }
+        }
       }else if( str[i] == bracketsConfig[k][0] ){
         count = 0;
         for( var j = i; j < sLength; j++ ){
@@ -24,7 +34,6 @@ module.exports = function check(str, bracketsConfig) {
           if( count == 0 && k == stack[stack.length-1] ){
             str = setCharAt(str,i,'0');
             str = setCharAt(str,j,'0');
-            //console.log(str);
             break;
           }
         }
@@ -34,10 +43,21 @@ module.exports = function check(str, bracketsConfig) {
     }
   }
 
+  //console.log(str);
   function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
   }
   
+  for( var k = 0; k < bracketsConfig.length; k++ ){
+    if( bracketsConfig[k][0] == bracketsConfig[k][1]){
+      for( var i = 0; i < sLength; i++ ){
+        if( str[i] == bracketsConfig[k][0] ){
+          return false;
+        }
+      }
+    }
+  }
+
   return true;
 }
